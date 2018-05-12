f945362b780990053de9b901aa305696ff752e49
Roger Hwang <hroger19@gmail.com>
2018-05-12 14:51:43 -0700

    an update1

--- test/test-server.js
+++ test/test-server.js
@@ -7,7 +7,7 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Shopping List', function() {
+describe('Blog posts', function() {
 
   before(function() {
     return runServer();                                           
@@ -23,8 +23,8 @@ describe('Shopping List', function() {
   		.then(function(res) {  
   			expect(res).to.have.status(200);
   			expect(res).to.be.json;
-        	expect(res.body).to.be.a('array');
-        	expect(res.body.length).to.be.at.least(1);
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(1);
 
         	const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
         	res.body.forEach(function(item) {
